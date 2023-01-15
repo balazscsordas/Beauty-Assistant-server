ae55de146297370d893008877717c7774a1234e4
Csordas Balazs <csordasbalu96@gmail.com>
2023-01-15 18:12:34 +0100

    cors settings changed

--- server.js
+++ server.js
@@ -48,6 +48,14 @@ app.listen(process.env.PORT, () => {
   console.log("Server is running on port " + process.env.PORT);
 })
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
+
 /* ROUTES */
 app.use("/auth", authRoutes);
 
