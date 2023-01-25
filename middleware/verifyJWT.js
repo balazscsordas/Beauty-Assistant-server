90dfd17ad0572029a9db2c394abd191749a139ff
Csordas Balazs <csordasbalu96@gmail.com>
2023-01-25 22:05:19 +0100

    middleware = 0

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -4,16 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const verifyJWT = (req, res, next) => {
-    const cookie = req.header('cookie');
-    const jwtToken = cookie.split('jwt=')[1];
-    jwt.verify(
-        jwtToken,
-        process.env.REFRESH_TOKEN_SECRET,
-        (err, decoded) => {
-            if (err) return res.sendStatus(403); //invalid token
-            req._id = decoded._id;
-        }
-    );
+    const adminId = '63d04da8ef25fa036e034d96';
     next();
 }
 
