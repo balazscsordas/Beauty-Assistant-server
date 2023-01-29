cf671e3d23908b3fdbbcc5cc7a85d7567313ee5d
Csordas Balazs <csordasbalu96@gmail.com>
2023-01-29 20:41:44 +0100

    cookie test 15

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -4,9 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const verifyJWT = (req, res, next) => {
-    req._id = '63d04da8ef25fa036e034d96';
-    /* const cookie = req.header('cookie');
-    const jwtToken = cookie.split('jwt=')[1];
+    /* req._id = '63d04da8ef25fa036e034d96'; */
+    const jwtToken = req.cookies.jwt;
+    if(!jwtToken) {
+        res.sendStatus(403);
+    }
     jwt.verify(
         jwtToken,
         process.env.REFRESH_TOKEN_SECRET,
@@ -14,7 +16,7 @@ const verifyJWT = (req, res, next) => {
             if (err) return res.sendStatus(403); //invalid token
             req._id = decoded._id;
         }
-    ); */
+    );
     next();
 }
 
