8d21e5b2b49e09cb816345870ed8ffff21ae0e3b
Csordas Balazs <csordasbalu96@gmail.com>
2023-01-29 15:24:42 +0100

    cookie test3

--- controllers/auth.js
+++ controllers/auth.js
@@ -41,7 +41,7 @@ export const login = async (req, res) => {
             firstName: foundUser.firstName,
             accessToken: accessToken,
         }
-        res.set('Set-Cookie', 'hehehehehe=asd')
+        res.set('Set-Cookie', 'hehehehehe=asd', {sameSite: 'None', secure: true })
         res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 });
         res.status(200).json({ authData, message: "Success" });
     } catch (err) {
--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -4,7 +4,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const verifyJWT = (req, res, next) => {
-    req._id = '63d04da8ef25fa036e034d96';
+    const cookie = req.header('cookie');
+    const jwtToken = cookie.split('jwt=')[1];
+    jwt.verify(
+        jwtToken,
+        process.env.REFRESH_TOKEN_SECRET,
+        (err, decoded) => {
+            if (err) return res.sendStatus(403); //invalid token
+            req._id = decoded._id;
+        }
+    );
     next();
 }
 
